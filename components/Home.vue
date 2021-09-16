6ff67323f44adb9acbb8e77585ec79ab991ae2ee
xirade <6myusic6@gmail.com>
2021-09-16 13:51:09 +0300

    optimization images

--- components/Home.vue
+++ components/Home.vue
@@ -31,8 +31,10 @@
         <section class="relative flex">
           <img
             class="w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="https://xirade.imgix.net/https%3A%2F%2Fres.cloudinary.com%2Fdxeebmzdv%2Fimage%2Fupload%2Fv1631721290%2Fnitay_upvtvm.png?s=f116eefed5cf1732abc81bba8b583715"
+            src="https://res.cloudinary.com/dxeebmzdv/image/upload/q_auto:good/v1631721290/nitay_upvtvm.webp"
             alt="Nitay Dor"
+            width="1511"
+            height="800"
           />
           <div
             class="absolute xl:left-1/4 lg:left-40 sm:left-14 left-10 inset-y-1/3 text-right"
@@ -79,6 +81,8 @@
             class="w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
             src="https://images.unsplash.com/photo-1459767129954-1b1c1f9b9ace?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80"
             alt="building"
+            width="1050"
+            height="700"
           />
           <div
             class="bg-gray-100 flex lg:flex-row flex-col items-start p-3 my-5"
@@ -107,14 +111,18 @@
             </div>
             <img
               class="w-full lg:h-full h-96 sm:h-84 object-contain"
-              src="@/assets/images/license.jpg"
+              src="@/assets/images/license.webp"
               alt="license"
+              width="331"
+              height="486"
             />
           </div>
           <img
             class="w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full mb-8"
             src="https://images.unsplash.com/photo-1579725942955-4d8377f8c66a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
-            alt="building"
+            alt="flat"
+            width="967"
+            height="725"
           />
         </article>
         <article id="contact" class="pb-5">
@@ -124,16 +132,16 @@
             action="#"
             class="mx-auto w-full md:w-3/4 lg:w-4/6 p-5"
           >
-          <transition name="slide-fade" v-if="success" appear>
-            <div
-              class="p-5 border border-red-400 bg-green-300 text-green-900 flex my-2"
-            >
-              <span class="mr-6">
-                <span class="absolute follows-links successfull"></span>
-              </span>
-              הטופס נשלח בהצלחה!
-            </div>
-          </transition>
+            <transition name="slide-fade" v-if="success" appear>
+              <div
+                class="p-5 border border-red-400 bg-green-300 text-green-900 flex my-2"
+              >
+                <span class="mr-6">
+                  <span class="absolute follows-links successfull"></span>
+                </span>
+                הטופס נשלח בהצלחה!
+              </div>
+            </transition>
             <div class="flex flex-col md:flex-row">
               <div class="flex-1">
                 <div class="p-3">
@@ -177,35 +185,41 @@
                 ></textarea>
               </div>
             </div>
-            <div class="p-3 pt-4 flex flex-col-reverse md:flex-row items-center justify-between">
+            <div
+              class="p-3 pt-4 flex flex-col-reverse md:flex-row items-center justify-between"
+            >
               <div class="mt-3 md:mt-0">
                 <a
                   target="_blank"
+                  rel="noreferrer"
                   href="https://facebook.com/nitay.dor"
-                  class=" p-2 border-2 border-gray-700 transition duration-500 ease-in-out hover:bg-gray-800 font-semibold text-white inline-flex items-center space-x-2"
+                  class="p-3 border-2 border-gray-700 transition duration-500 ease-in-out hover:bg-gray-800 font-semibold text-white inline-flex items-center space-x-2"
                 >
                   <svg
-                    class="w-5 h-5"
+                    class="w-6 h-6"
                     role="img"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                   >
+                  <title>Facebook</title>
                     <path
                       d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
                     />
                   </svg>
                 </a>
                 <a
+                  rel="noreferrer"
                   href="https://www.instagram.com/nitay_dor/"
                   target="_blank"
-                  class="p-2 border-2 border-gray-700 transition duration-500 ease-in-out hover:bg-gray-800 font-semibold text-white inline-flex items-center space-x-2"
+                  class="p-3 border-2 border-gray-700 transition duration-500 ease-in-out hover:bg-gray-800 font-semibold text-white inline-flex items-center space-x-2"
                 >
                   <svg
-                    class="w-5 h-5"
+                    class="w-6 h-6"
                     role="img"
                     viewBox="0 0 150 150"
                     xmlns="http://www.w3.org/2000/svg"
                   >
+                  <title>Instagram</title>
                     <path
                       d="M41.668 0C18.6914 0 0 18.6914 0 41.668V108.332C0 131.309 18.6914 150 41.668 150H108.332C131.309 150 150 131.309 150 108.332V41.668C150 18.6914 131.309 0 108.332 0H41.668ZM41.668 16.668H108.332C122.117 16.668 133.332 27.8828 133.332 41.668V108.332C133.332 122.117 122.117 133.332 108.332 133.332H41.668C27.8828 133.332 16.668 122.117 16.668 108.332V41.668C16.668 27.8828 27.8828 16.668 41.668 16.668ZM116.668 25C116.121 25 115.578 25.0547 115.039 25.1602C114.504 25.2656 113.984 25.4258 113.477 25.6328C112.973 25.8438 112.492 26.1016 112.035 26.4062C111.582 26.707 111.16 27.0547 110.773 27.4414C110.387 27.8281 110.043 28.25 109.738 28.7031C109.434 29.1602 109.176 29.6406 108.969 30.1445C108.758 30.6484 108.602 31.1719 108.492 31.707C108.387 32.2461 108.332 32.7852 108.332 33.332C108.332 33.8789 108.387 34.4219 108.492 34.9609C108.602 35.4961 108.758 36.0156 108.969 36.5234C109.176 37.0273 109.434 37.5078 109.738 37.9648C110.043 38.418 110.387 38.8398 110.773 39.2266C111.16 39.6133 111.582 39.957 112.035 40.2617C112.492 40.5664 112.973 40.8242 113.477 41.0312C113.984 41.2422 114.504 41.3984 115.039 41.5078C115.578 41.6133 116.121 41.668 116.668 41.668C117.215 41.668 117.754 41.6133 118.293 41.5078C118.828 41.3984 119.352 41.2422 119.855 41.0312C120.359 40.8242 120.84 40.5664 121.297 40.2617C121.75 39.957 122.172 39.6133 122.559 39.2266C122.945 38.8398 123.293 38.418 123.594 37.9648C123.898 37.5078 124.156 37.0273 124.367 36.5234C124.574 36.0156 124.734 35.4961 124.84 34.9609C124.945 34.4219 125 33.8789 125 33.332C125 32.7852 124.945 32.2461 124.84 31.707C124.734 31.1719 124.574 30.6484 124.367 30.1445C124.156 29.6406 123.898 29.1602 123.594 28.7031C123.293 28.25 122.945 27.8281 122.559 27.4414C122.172 27.0547 121.75 26.707 121.297 26.4062C120.84 26.1016 120.359 25.8438 119.855 25.6328C119.352 25.4258 118.828 25.2656 118.293 25.1602C117.754 25.0547 117.215 25 116.668 25ZM75 33.332C52.0234 33.332 33.332 52.0234 33.332 75C33.332 97.9766 52.0234 116.668 75 116.668C97.9766 116.668 116.668 97.9766 116.668 75C116.668 52.0234 97.9766 33.332 75 33.332ZM75 50C88.7852 50 100 61.2148 100 75C100 88.7852 88.7852 100 75 100C61.2148 100 50 88.7852 50 75C50 61.2148 61.2148 50 75 50Z"
                     />
@@ -251,7 +265,7 @@
             >
               <p>
                 Website Design by
-                <a href="https://xifolio.com/" target="_blank">Xirade</a>
+                <a href="https://xifolio.com/" rel="noreferrer" target="_blank">Xirade</a>
               </p>
               <div class="flex space-s-6 py-3 text-gray-900 font-medium">
                 <a
@@ -279,11 +293,12 @@
       </footer>
       <a
         href="tel:0525357888"
-        class="block w-14 h-14 transition duration-500 ease-in-out  border-2 p-2 bg-gray-800 hover:bg-gray-50 border-gray-700 chat-btn rounded-full"
+        class="block w-14 h-14 transition duration-500 ease-in-out  border-2 bg-gray-800 hover:bg-gray-50 border-gray-700 chat-btn rounded-full"
       >
         <img
           class="absolute h-8 w-8 top-2.5 left-2.5"
           src="@/assets/images/phone.svg"
+          alt="phone"
         />
       </a>
     </div>
@@ -297,12 +312,12 @@ export default {
     email: "",
     tel: "",
     message: "",
-    messages:[],
+    messages: [],
     pending: false,
     success: false
   }),
   methods: {
-     send() {
+    send() {
       this.messages = [];
       this.triggerSendMessageFunction();
     },
@@ -360,7 +375,7 @@ q::before {
   width: 40px;
   height: 40px;
   background-size: contain;
-  background-repeat: no-repeat ;
+  background-repeat: no-repeat;
   background-image: url("@/assets/images/quotes.svg");
 }
 q::after {
