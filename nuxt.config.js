871626b1d8e62961096057fe8668e5183888200b
xirade <6myusic6@gmail.com>
2021-09-19 00:47:02 +0300

    adding meta tags and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,18 +4,20 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "נתאי דור - יועץ נדל''ן",
     htmlAttrs: {
       lang: "he"
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/png", sizes:"16x16", href: "/favicon-16x16.png" },
+      { rel: "icon", type: "image/png", sizes:"32x32",href: "/favicon-32x32.png" },
+      { rel: "apple-touch-icon", sizes:"180x180", href: "/apple-touch-icon.png" },
+      { rel: "manifest", href: "/site.webmanifest" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       {
         rel: "preconnect",
--- pages/index.vue
+++ pages/index.vue
@@ -6,18 +6,30 @@
 export default {
   data() {
     return {
+      title: "נתאי דור - יועץ נדל''ן",
       img:
         "https://res.cloudinary.com/dxeebmzdv/image/upload/q_auto:good/v1631721290/nitay_upvtvm.webp"
     };
   },
   head() {
     return {
+      title: this.title,
       meta: [
+        {
+          hid: "author",
+          name: "author",
+          content: "נתאי דור"
+        },
         {
           hid: "description",
           name: "description",
           content:
             "האיש שלך לבית חלומותך! התמחות בבתי מגורים, ניתוח סביבה קהילתית להתאמה אישית, אפיון והתאמה של הנכס בהתאם למיקומו ואומדנו ליווי אישי בתהליכי רכישה/ מכירה."
+        },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary_large_image"
         },
         {
           hid: "twitter:title",
@@ -65,6 +77,46 @@ export default {
           hid: "og:image:alt",
           property: "og:image:alt",
           content: "נתאי דור - יועץ נדל''ן"
+        },
+        {
+          hid: "og:image:width",
+          property: "og:image:width",
+          content: "1920"
+        },
+        {
+          hid: "og:image:height",
+          property: "og:image:alt",
+          content: "1012"
+        },
+        {
+          hid: "og:image:type",
+          property: "og:image:type",
+          content: "image/webp"
+        },
+        {
+          hid: "og:type",
+          property: "og:type",
+          content: "article"
+        },
+        {
+          hid: "og:locale",
+          property: "og:locale",
+          content: "he_IL"
+        },
+        {
+          hid: "og:site_name",
+          property: "og:site_name",
+          content: "נתאי דור - יועץ נדל''ן"
+        },
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "http://nitaydor-nadlan.com/"
+        },
+        {
+          hid: "robots",
+          property: "robots",
+          content: "index,follow"
         }
       ]
     };
