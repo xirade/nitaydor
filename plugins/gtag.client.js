c929cd2f6eb5b571ff4c91dbb875e7b1f7d6919b
xirade <6myusic6@gmail.com>
2021-09-21 14:31:57 +0300

    fixed google analyst

---
+++ plugins/gtag.client.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import VueGtag from "vue-gtag";
+
+export default ({ isDev, app }) => {
+	if (!isDev){
+		Vue.use(VueGtag, {
+			config: { id: process.env.GOOGLE_ANALYTICS_ID }
+		},
+		app.router
+		);
+	} else {
+		console.log("Skipping GA in development")
+	}
+}
--- plugins/vue-gtag.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import VueGtag from "vue-gtag";
-
-const API_KEY = process.env.GOOGLE_ANALYTICS_ID
-
-Vue.use(VueGtag, {
-  config: { id: API_KEY},
-})
