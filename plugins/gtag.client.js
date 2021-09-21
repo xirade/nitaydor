af31156c5ed09ad3df75df479e24bd723c348b89
xirade <6myusic6@gmail.com>
2021-09-21 14:38:06 +0300

    fixed google analyst

--- plugins/gtag.client.js
+++ plugins/gtag.client.js
@@ -4,7 +4,7 @@ import VueGtag from "vue-gtag";
 export default ({ isDev, app }) => {
 	if (!isDev){
 		Vue.use(VueGtag, {
-			config: { id: process.env.GOOGLE_ANALYTICS_ID }
+			config: { id: `${process.env.GOOGLE_ANALYTICS_ID}` }
 		},
 		app.router
 		);
